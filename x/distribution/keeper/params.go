6da5e504fd990374ac439bef4be04ddf9b652552
Mantrachain Support <security@mantrachain.io>
2024-09-16 14:44:44 +0800

    add a getter for the maca pool tax

--- x/distribution/keeper/params.go
+++ x/distribution/keeper/params.go
@@ -16,6 +16,16 @@ func (k Keeper) GetCommunityTax(ctx context.Context) (math.LegacyDec, error) {
 	return params.CommunityTax, nil
 }
 
+// GetMcaTax returns the current distribution mca tax.
+func (k Keeper) GetMcaTax(ctx context.Context) (math.LegacyDec, error) {
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		return math.LegacyDec{}, err
+	}
+
+	return params.McaTax, nil
+}
+
 // GetWithdrawAddrEnabled returns the current distribution withdraw address
 // enabled parameter.
 func (k Keeper) GetWithdrawAddrEnabled(ctx context.Context) (enabled bool, err error) {
