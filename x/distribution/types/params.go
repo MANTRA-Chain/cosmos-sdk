2443c9d2aedf43eb0479f9cb752de9c5dc228614
Mantrachain Support <security@mantrachain.io>
2024-09-16 15:27:33 +0800

    fixes

--- x/distribution/types/params.go
+++ x/distribution/types/params.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 // DefaultParams returns default distribution parameters
@@ -13,13 +14,26 @@ func DefaultParams() Params {
 		BaseProposerReward:  math.LegacyZeroDec(),            // deprecated
 		BonusProposerReward: math.LegacyZeroDec(),            // deprecated
 		WithdrawAddrEnabled: true,
-		McaTax:              math.LegacyNewDecWithPrec(40, 2), // 40%
+		McaTax:              math.LegacyNewDecWithPrec(40, 2),                // 40%
+		McaAddress:          "mantra15m77x4pe6w9vtpuqm22qxu0ds7vn4ehzwx8pls", // TODO: change this to the actual mca address
 	}
 }
 
 // ValidateBasic performs basic validation on distribution parameters.
 func (p Params) ValidateBasic() error {
-	return validateCommunityTax(p.CommunityTax)
+	err := validateCommunityTax(p.CommunityTax)
+	if err != nil {
+		return err
+	}
+	err = validateMcaTax(p.McaTax)
+	if err != nil {
+		return err
+	}
+	err = validateMcaAddress(p.McaAddress)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func validateCommunityTax(i interface{}) error {
@@ -55,6 +69,20 @@ func validateMcaTax(i interface{}) error {
 	}
 	if v.GT(math.LegacyOneDec()) {
 		return fmt.Errorf("mca tax too large: %s", v)
+	}
+
+	return nil
+}
+
+func validateMcaAddress(i interface{}) error {
+	v, ok := i.(string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	_, err := sdk.AccAddressFromBech32(v)
+	if err != nil {
+		return fmt.Errorf("invalid MCA address: %s", err)
 	}
 
 	return nil
